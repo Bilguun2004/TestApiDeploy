3e523d3712bbbc8fd5c2a063d595adb444137e81
bilguun8689 <bilguun8689@gmail.com>
2024-01-11 11:41:12 +0800

    update

--- index.js.js
+++ index.js.js
@@ -1,20 +1,48 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const axios = require("axios");
 const port = 3000;
 
-// Define a sample API endpoint
+app.use(express.json());
+
+const Numerology = async (paramValue) => {
+	try {
+		const options = {
+			method: "GET",
+			url: "https://horoscope-astrology.p.rapidapi.com/numerology",
+			params: paramValue,
+			headers: {
+				"X-RapidAPI-Key": "13f0abee3amshdcb6be2d033767bp191d2djsn824c023866eb",
+				"X-RapidAPI-Host": "horoscope-astrology.p.rapidapi.com",
+				"Content-Type": "application/json",
+			},
+		};
+		return axios.request(options);
+	} catch (error) {
+		console.error(error);
+		throw error;
+	}
+};
+
 app.use(cors());
-app.get("/api/sample", (req, res) => {
-	const data = {
-		message: "Hello, this is a sample API!",
-		timestamp: new Date().toISOString(),
-	};
-	console.log(data);
-	res.json(data);
+app.post("/api/sample", async (req, res) => {
+	try {
+		const paramValue = req.body;
+		const response = await Numerology(paramValue);
+		console.log(paramValue);
+
+		const data = {
+			message: response.data.desc,
+		};
+
+		res.json(data);
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({error: "Internal Server Error"});
+	}
 });
 
-// Start the server
 app.listen(port, () => {
 	console.log(`Server is running on http://localhost:${port}`);
 });
